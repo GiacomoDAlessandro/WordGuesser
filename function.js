fb5bd8ea2c56798f39e1f0699098426409ac75ee
GiacomoDAlessandro <giacomo.dl04@gmail.com>
2025-11-07 16:30:48 -0600

    added past guess boxes and fixed some writing issues

--- function.js
+++ function.js
@@ -1,17 +1,35 @@
-const bigBox = document.querySelector(".big-box");
+document.addEventListener('DOMContentLoaded', function () {
 
-for (let i = 0; i < 6; i++) {
-    const row = document.createElement("div");
-    row.className = "guess-row"
 
-    for (let j = 0; j < 5; j++) {
-        const box = document.createElement("div");
-        box.className = "past-letter-box";
-        row.appendChild(box);
+    const bigBox = document.querySelector(".big-box");
+
+    for (let i = 0; i < 6; i++) {
+        const row = document.createElement("div");
+        row.className = "guess-row"
+
+        for (let j = 0; j < 5; j++) {
+            const box = document.createElement("div");
+            box.className = "past-letter-box";
+            row.appendChild(box);
+        }
+
+        bigBox.appendChild(row);
     }
 
-    bigBox.appendChild(row);
-}
+    document.querySelectorAll('.letter-box').forEach((box, index, boxes) => {
+        box.addEventListener('input', () => {
+            if (box.value.length === 1 && index < boxes.length - 1) {
+                boxes[index + 1].focus();
+            }
+        })
+
+        box.addEventListener('keydown', (e) => {
+            if (e.key === 'Backspace' && box.value === '' && index > 0) {
+                boxes[index - 1].focus();
+            }
+        })
+    })
+});
 
 function buttonClicked() {
 
